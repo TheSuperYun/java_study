facfaba7f88dde25f0b33f0ce57596224676c993
TheSuperYun <134270342+TheSuperYun@users.noreply.github.com>
2023-10-28 17:12:59 +0800

    Add files via upload

---
+++ ArithmeticianDemo1.java
@@ -0,0 +1,11 @@
+package com.ithema.arithmeticoperato;
+
+public class ArithmeticianDemo1 {
+    public static void main(String[] args) {
+        System.out.println(3 + 2);
+        System.out.println(5 - 1);
+        System.out.println(1.1 + 1.01);
+        System.out.println(1.1-1.01);
+        System.out.println(1.1*1.01);
+    }
+}
