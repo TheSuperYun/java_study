80388c257f8f2cdac619ace49a7e6aa61edbaae4
TheSuperYun <134270342+TheSuperYun@users.noreply.github.com>
2023-10-29 16:23:06 +0800

    Add files via upload

---
+++ for_dome_1.java
@@ -0,0 +1,13 @@
+package com.ithema.arithmeticoperato;
+
+import java.util.Scanner;
+
+public class for_dome_1 {
+    public static void main(String[] args) {
+        int sum =0;
+        for (int i = 1; i <=5 ; i++) {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
